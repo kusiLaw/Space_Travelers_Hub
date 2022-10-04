a259a9385b4714240cc0237688dbaa95d0f41e0d
Bateyjosue <josuebatey19@gmail.com>
2022-10-04 16:42:59 +0200

    Add the Dragon Route Element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import Dragon from './Components/Pages/Dragons';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/dragon" element={<Dragon />} />
+      </Routes>
+    </>
   );
 }
 
