45b901b74a45d9b967f7281cc68caa14e3e5faf7
Farida-Hamid <faridaaddala@gmail.com>
2022-10-04 17:18:44 +0300

    Add reducer

--- src/Components/Redux/missions.js
+++ src/Components/Redux/missions.js
@@ -6,3 +6,15 @@ export const read = (missions) => ({
   type: READ,
   payload: missions,
 });
+
+// Reducer
+const missionsReducer = (state = [], action) => {
+  switch (action.type) {
+    case READ:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
