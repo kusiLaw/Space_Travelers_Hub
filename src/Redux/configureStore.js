4a0d6d8ac333274128c09efd4958d80ee8d779a5
Farida-Hamid <faridaaddala@gmail.com>
2022-10-05 12:15:30 +0300

    Fix missions dragons bug

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from '@reduxjs/toolkit'
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionsReducer from './Missions/missions';
+import { reducerDragons } from './Dragons/Dragons';
 
 const rootReducer = combineReducers(
   {
     missions: missionsReducer,
+    dragons: reducerDragons,
   },
 );
 
