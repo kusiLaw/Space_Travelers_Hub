97f8373cf5afdaaa1b0fa2e2b0c089c001a3aee6
Farida-Hamid <faridaaddala@gmail.com>
2022-10-06 17:34:20 +0300

    Test my profile component

---
+++ src/__tests__/myProfile.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import MyProfile from "../Components/Pages/myProfile";
+import store from '../Redux/configureStore';
+
+it('Testing my profile component:', () => {
+  const tree = TestRenderer.create(
+    <Provider store={store}>
+      <MyProfile />
+    </Provider>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
